0f92a5529d2ec8a10e089fd359b927bcf00e75f4
Sander0542 <Sander0542@users.noreply.github.com>
2021-10-24 12:02:09 +0000

    deploy: 27c6c349de1c64a4b68ae7ce17fdac5107b8a919

--- search/all_0.js
+++ search/all_0.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['a_0',['A',['../classspic_1_1Color.html#a0909b8c7bf2b4f6aecd41e3c45e8621a',1,'spic::Color']]],
   ['active_1',['Active',['../classspic_1_1Component.html#a0240ed3c3632a2fa08b90796c0600020',1,'spic::Component::Active() const'],['../classspic_1_1Component.html#a3804f5e3cac204184980b7e34e991033',1,'spic::Component::Active(bool flag)']]],
-  ['addcomponent_2',['AddComponent',['../classspic_1_1GameObject.html#a7f3ad9190c3aa82382a6dd2c98261c1d',1,'spic::GameObject']]],
+  ['addcomponent_2',['AddComponent',['../classspic_1_1GameObject.html#aed4e802151d6274b28edb997f61fab40',1,'spic::GameObject']]],
   ['addforce_3',['AddForce',['../classspic_1_1RigidBody.html#ab97cfa653ee725ea97574dca9469acc1',1,'spic::RigidBody']]],
   ['animator_4',['Animator',['../classspic_1_1Animator.html#a79fa7a3c4485a42ee32fad014c656fd3',1,'spic::Animator::Animator()'],['../classspic_1_1Animator.html',1,'spic::Animator']]],
   ['anykey_5',['AnyKey',['../namespacespic_1_1Input.html#ad29cd24d375d286a6d45cd89adc529f1',1,'spic::Input']]],
--- search/functions_4.js
+++ search/functions_4.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['find_152',['Find',['../classspic_1_1GameObject.html#a468250d6be475c50c0a0466dd2740f24',1,'spic::GameObject']]],
   ['findgameobjectswithtag_153',['FindGameObjectsWithTag',['../classspic_1_1GameObject.html#a8a856ffd67023ae5dd55e2e401684f81',1,'spic::GameObject']]],
-  ['findobjectoftype_154',['FindObjectOfType',['../classspic_1_1GameObject.html#abe9b817a231613c65d08cf59200d5605',1,'spic::GameObject']]],
-  ['findobjectsoftype_155',['FindObjectsOfType',['../classspic_1_1GameObject.html#aa60eac0d485f371b175d26c158c6768e',1,'spic::GameObject']]],
+  ['findobjectoftype_154',['FindObjectOfType',['../classspic_1_1GameObject.html#a39c116d50efe7c3b7eb7be8be161be4c',1,'spic::GameObject']]],
+  ['findobjectsoftype_155',['FindObjectsOfType',['../classspic_1_1GameObject.html#a04cc55e1ade46360ba9eb45a7c499274',1,'spic::GameObject']]],
   ['findwithtag_156',['FindWithTag',['../classspic_1_1GameObject.html#afe1650b6c75098e9b63a0ccece558567',1,'spic::GameObject']]],
   ['font_157',['Font',['../classspic_1_1Text.html#a302d8a0d8fcbccea485c9c9fdc8dd150',1,'spic::Text::Font() const'],['../classspic_1_1Text.html#a4819255c817e6f51e8ee0da5137271ec',1,'spic::Text::Font(const std::string &amp;font)']]]
 ];
