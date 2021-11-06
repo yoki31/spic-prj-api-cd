ec06e0f99f5e85ac2b431f18f89e11cd13052e06
stevenliebregt <stevenliebregt@users.noreply.github.com>
2021-11-06 12:13:26 +0000

    deploy: b5f4f01c12ab872c6034ce0336c99f5b2baa7b6d

--- search/functions_e.js
+++ search/functions_e.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['setcolor_265',['SetColor',['../classspic_1_1Color.html#afb0df0bae7b0d2ab2c48e7bfa42cbf0a',1,'spic::Color::SetColor(double r, double g, double b)'],['../classspic_1_1Color.html#aca4293f70e6979b027289545b8d5b546',1,'spic::Color::SetColor(double r, double g, double b, double a)']]],
+  ['setcolor_265',['SetColor',['../classspic_1_1Color.html#a78ca32414237f854b08c3b75dba58dbf',1,'spic::Color::SetColor(double red, double green, double blue)'],['../classspic_1_1Color.html#ae8bc097e0575943b14345fdcaf29cf8e',1,'spic::Color::SetColor(double red, double green, double blue, double alpha)']]],
   ['size_266',['Size',['../classspic_1_1Text.html#adbd1abe4606aa24f1b34e94c1a308b3a',1,'spic::Text::Size() const'],['../classspic_1_1Text.html#aff1a68416fe5b84e8c1fc329c63dd0cd',1,'spic::Text::Size(int size)']]],
   ['sortinglayer_267',['SortingLayer',['../classspic_1_1Sprite.html#a2884a2dbe3b667b82139278508a11783',1,'spic::Sprite::SortingLayer(int sortingLayer)'],['../classspic_1_1Sprite.html#af6282045f1472366641d4aece5bed307',1,'spic::Sprite::SortingLayer() const']]],
   ['sprite_268',['Sprite',['../classspic_1_1Sprite.html#af77fcf44d3bc02efbdf1d66b8bd8010c',1,'spic::Sprite::Sprite(const std::string &amp;sprite, bool flipX, bool flipY, int sortingLayer, int orderLayer)'],['../classspic_1_1Sprite.html#ade8fcad6447e8c0dff43ddbcc5a7542d',1,'spic::Sprite::Sprite(const std::string &amp;sprite, const Color &amp;color, bool flipX, bool flipY, int sortingLayer, int orderInLayer)']]],
