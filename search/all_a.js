18d316000700bdb982e769d83c4f68eeab2f8585
daanh432 <daanh432@users.noreply.github.com>
2021-11-12 16:34:42 +0000

    deploy: ab93e2ae3b77bcc366827212421b20ce6a06054f

--- search/all_a.js
+++ search/all_a.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['layer_70',['Layer',['../classspic_1_1GameObject.html#ab8bea5d873e207bacb7268c3e37d46bd',1,'spic::GameObject']]],
   ['lime_71',['lime',['../classspic_1_1Color.html#adec6e038435ecd6244254fba864a8169',1,'spic::Color']]],
-  ['log_72',['Log',['../namespacespic_1_1Debug.html#a5eb926c42e2c88c5c7be6d46e8276ad2',1,'spic::Debug']]],
-  ['logerror_73',['LogError',['../namespacespic_1_1Debug.html#ab4747e7b714195926186f351ad74910d',1,'spic::Debug']]],
-  ['logwarning_74',['LogWarning',['../namespacespic_1_1Debug.html#afc8c8fee30630ffc7fdbd74baaef66cd',1,'spic::Debug']]]
+  ['log_72',['Log',['../namespacespic_1_1Debug.html#a38eed6ad571132cc72a3ab9676a2d86b',1,'spic::Debug']]],
+  ['logerror_73',['LogError',['../namespacespic_1_1Debug.html#ae1efcb45c89d90a6603e143d9647e60e',1,'spic::Debug']]],
+  ['logwarning_74',['LogWarning',['../namespacespic_1_1Debug.html#ad88ed4d0178efc4134b8a827975aa928',1,'spic::Debug']]]
 ];
